ac278a35cd6abb88cf71a573b5cf7982d079c882
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-01-07 22:46:01 +0900

    과제 1번 코드 재작성

--- src/main/java/com/task/brique_task/controller/AgainController.java
+++ src/main/java/com/task/brique_task/controller/AgainController.java
@@ -1,8 +1,12 @@
 package com.task.brique_task.controller;
 
-import com.task.brique_task.config.SocketService;
+import ch.qos.logback.core.status.StatusUtil;
 import com.task.brique_task.employ.service.EmployService;
 import com.task.brique_task.util.CsvReader;
+
+import org.apache.commons.math3.stat.StatUtils;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -15,47 +19,57 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.*;
-import java.net.HttpURLConnection;
 import java.net.Socket;
-import java.net.URL;
-import java.net.UnknownHostException;
 import java.util.*;
 
 @Controller
 @RequestMapping(value = "/")
-public class AgainContoller {
+public class AgainController {
 
     @Autowired
     @Qualifier("employService")
     private EmployService employService;
-
 
     final static String SERVER_IP = "127.0.0.1";
     final static int SERVER_PORT = 56768;
 
     @RequestMapping("/task1")
     public String csvFile(Model model) {
-       /* CsvReader csv = new CsvReader(); // csvRead 객체 생성
+        CsvReader csv = new CsvReader(); // csvRead 객체 생성
         List<List<String>> list = csv.readCSV(); // 읽은 데이터 list에 초기화
-        List<List<Double>> result = new ArrayList<>(); // 데이터를 저장할 List 생성
-        String text = ""; // 숫자가 아닌 값들의 모음
-        BufferedReader in = null;
-
-        try {
-            URL url = new URL("https://drive.google.com/file/d/1Ah0gkauGCIqJHpFGhTgsEZCjYFRscjTh/view?usp=sharing");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
-            String line = "";
-            while ((line = in.readLine()) != null) {
-                System.out.println("line : " + line);
+        List<List<Double>> result = new ArrayList<>();
+        int totalLine = list.size();
+        int calculLine = 0;
+        String errorMsg = "";
+        for (int i = 1; i < list.size(); i++) {
+            try {
+                double[] doubles = Arrays.stream(list.get(i).toString().substring(1, list.get(i).toString().length() - 1).replaceAll(" ", "").split(","))
+                        .mapToDouble(Double::parseDouble)
+                        .toArray();
+                DescriptiveStatistics stats = new DescriptiveStatistics(doubles);
+                double min = StatUtils.min(doubles);
+                double max = StatUtils.max(doubles);
+                double sum = StatUtils.sum(doubles);
+                double avg = StatUtils.mean(doubles);
+                double std = stats.getStandardDeviation();
+                double median = stats.getPercentile(50);
+                calculLine++; // 계산된 라인 카운트
+                List resultLine = new ArrayList();
+                resultLine.add(min);
+                resultLine.add(max);
+                resultLine.add(sum);
+                resultLine.add(avg);
+                resultLine.add(std);
+                resultLine.add(median);
+                result.add(resultLine);
+            } catch (Exception e) {
+                errorMsg += list.get(i).toString().replaceAll("[^a-zA-Z]","");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-
-            model.addAttribute("list", result);
-            model.addAttribute("text", text);*/
+        model.addAttribute("errorMsg", errorMsg);
+        model.addAttribute("totalLine", totalLine);
+        model.addAttribute("calculLine", calculLine);
+        model.addAttribute("list", result);
         return "task1";
 
     }
@@ -72,20 +86,20 @@ public class AgainContoller {
         Socket socket = null;
 
         try {
-            socket = new Socket(SERVER_IP,SERVER_PORT);
+            socket = new Socket(SERVER_IP, SERVER_PORT);
             System.out.println("socket 연결");
 
-            /**	Client에서 Server로 보내기 위한 통로 */
+            /**    Client에서 Server로 보내기 위한 통로 */
             OutputStream os = socket.getOutputStream();
-            /**	Server에서 보낸 값을 받기 위한 통로 */
+            /**    Server에서 보낸 값을 받기 위한 통로 */
             InputStream is = socket.getInputStream();
 
-            os.write( content.getBytes() );
+            os.write(content.getBytes());
             os.flush();
 
             byte[] data = new byte[16];
             int n = is.read(data);
-            final String resultFromServer = new String(data,0,n);
+            final String resultFromServer = new String(data, 0, n);
 
             System.out.println(resultFromServer);
             socket.close();
@@ -94,7 +108,6 @@ public class AgainContoller {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("처리 중 오류가 발생하였습니다.");
         }
-
     }
 
 
